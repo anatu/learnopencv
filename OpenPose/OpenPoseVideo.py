53eb4e26098d423073390c1e6e63ad29f33e5722
anatu <natu.anand@gmail.com>
2019-02-18 23:28:22 -0800

    Added in function to dump points data into CSV file

--- OpenPose/OpenPoseVideo.py
+++ OpenPose/OpenPoseVideo.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import numpy as np
+import os
+import itertools
 
 # Notes
 # EDGE DETECTION FOR VIDEO: https://www.geeksforgeeks.org/real-time-edge-detection-using-opencv-python/
@@ -10,9 +12,16 @@ class Skeletonizer():
 
 	def set_mode(self, mode):
 		# Specifies the type of model to use.
+		
 		# The protoFile defines the architecture of the neural net (how layers are arranged)
+		
 		# The .caffemodel file stores the pre-trained weights of the model
+		
 		# n_points is the number of data points used in that training dataset
+		
+		# kpNames are the names of the body parts intended to be mapped by each keypoint
+		# as described in https://www.learnopencv.com/deep-learning-based-human-pose-estimation-using-opencv-cpp-python/
+
 		# MPII and COCO are two different training datasets
 
 		# Model architecture for VGGNet is as follows:
@@ -27,12 +36,16 @@ class Skeletonizer():
 		    self.protoFile = "pose/coco/pose_deploy_linevec.prototxt"
 		    self.weightsFile = "pose/coco/pose_iter_440000.caffemodel"
 		    self.nPoints = 18
+		    self.kpNames = ["Nose", "Shoulder_R", "Elbow_R", "Wrist_R", "Shoulder_L", "Elbow_L", "Wrist_L", \
+		    "Hip_R", "Knee_R", "Ankle_R", "Hip_L", "Knee_L", "Ankle_L", "Eye_R", "Eye_L", "Ear_R", "Ear_L", "Bkgrd"]
 		    self.POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
 
 		elif MODE is "MPI" :
 		    self.protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
 		    self.weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
 		    self.nPoints = 15
+		    self.kpNames = ["Head", "Neck", "Shoulder_R", "Elbow_R", "Wrist_R", "Shoulder_L", "Elbow_L", "Wrist_L",\
+		    "Hip_R", "Knee_R", "Ankle_R", "Hip_L", "Knee_L", "Ankle_L", "Chest", "Background"]
 		    self.POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
 
 
@@ -45,8 +58,15 @@ class Skeletonizer():
 		threshold = 0.1
 		return inWidth, inHeight, threshold
 
+	def prepare_out_data_file(self, outname):
+		outfile = open(outname, "w")
+		kp_X = [elem + "_X" for elem in self.kpNames]
+		kp_Y = [elem + "_Y" for elem in self.kpNames]
+		join = list(zip(kp_X, kp_Y))
+		outfile.write(",".join(list(sum(join, ()))) + "\n")
+		return outfile
 
-	def track_video_from_file(self, filename):
+	def track_video_from_file(self, filename, outname):
 
 		self.set_mode("MPI")
 		inWidth, inHeight, threshold = self.set_params()
@@ -57,9 +77,11 @@ class Skeletonizer():
 		# cap = cv2.VideoCapture(0)
 		hasFrame, frame = cap.read()
 		vid_writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
-
 		# Load the neural network using the prototxt architecture spec and the pre-trained weights
 		net = cv2.dnn.readNetFromCaffe(self.protoFile, self.weightsFile)
+
+		# Prepare the file to store points
+		outfile = self.prepare_out_data_file(outname)
 
 		while cv2.waitKey(1) < 0:
 		    t = time.time()
@@ -102,6 +124,10 @@ class Skeletonizer():
 		        else :
 		            points.append(None)
 
+		    flatPoints = [str(item) for sublist in points for item in sublist]
+		    outfile.write(",".join(flatPoints) + "\n")
+
+
 		    # Draw Skeleton
 		    for pair in self.POSE_PAIRS:
 		        partA = pair[0]
@@ -118,6 +144,7 @@ class Skeletonizer():
 		    cv2.imshow('Output-Skeleton', frame)
 
 		    vid_writer.write(frame)
+		outfile.close()
 		vid_writer.release()
 
 
@@ -137,4 +164,4 @@ class Skeletonizer():
 
 if __name__ == "__main__":
 	skel = Skeletonizer()
-	skel.track_video_from_file("arm_motions.mp4")
+	skel.track_video_from_file("reddy_video.mp4", "output_points.csv")
