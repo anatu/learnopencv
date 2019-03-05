c9e54260c6468742add20812e3b6dd26f0edb69b
Anand Natu <natu.anand@gmail.com>
2019-03-04 20:22:12 -0800

    Added test node for tree

--- NodDetection/test.py
+++ NodDetection/test.py
@@ -1,8 +1,20 @@
+class Node():
+	def __init__(self, media, yChild, nChild):
+		self.media = media
+		self.yChild = yChild
+		self.nChild = nChild
+		# if (self.yChild == None or self.nChild == None) and (self.yChild != self.nChild):
+		# 	raise AssertionError("Can't have only one None child")
+
+	def __repr__(self):
+		result = "Media: %s, Yes_Child: %s, No_Child: %s" % (self.media, self.yChild, self.nChild)
+		return result
+
 def build_decision_tree():
-	tree = dict()
-	
+	tree = Node(1, "B", "C")
 	return tree
 
 
 if __name__ == "__main__":
-	tree = build_decision_tree()
+	node = Node(1,2,3)
+	print(node)
